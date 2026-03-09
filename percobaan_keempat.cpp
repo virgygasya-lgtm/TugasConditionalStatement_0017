42cf2f9d078a3b70785be95f311c8b5892043bba
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-03-09 22:26:24 +0700

    percobaan_keempat.cpp

---
+++ percobaan_keempat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    return 0;
+}
---
+++ percobaan_kelima.cpp
@@ -0,0 +1,8 @@
+void inputData(float &berat, float &tinggi)
+{
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
