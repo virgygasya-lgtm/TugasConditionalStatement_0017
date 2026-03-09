59f3f3d6848610efc4651ee58cd818cd018b020b
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-03-09 22:19:29 +0700

    percobaan_pertama.cpp

---
+++ percobaan_ketiga.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    return 0;
+}
---
+++ percobaan_pertama.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Prosedur untuk input data
+void inputData(float & berat, float & tinggi)
+{
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
+// Fungsi menghitung BMI
+float hitungBMI(float berat, float tinggi)
+{
+    return berat / (tinggi * tinggi);
+}
+
+// Fungsi menentukan status BMI
+string statusBMI(float bmi)
+{
+    if(bmi < 18.5)
+    {
+        return "Berat Badan Kurang";
+    }
+    else if(bmi < 25)
+    {
+        return "Berat Badan Normal";
+    }
+    else if(bmi < 30)
+    {
+        return "Berat Badan Kelebihan";
+    }
+    else{
+        return "Obesitas";
+    }
+}
+
+int main()
+{
+
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    // Memanggil prosedur input
+    inputData(berat, tinggi);
+
+    // Menghitung BMI
+    bmi = hitungBMI(berat, tinggi);
+
+    cout << endl;
+    cout << "--- Hasil ---" << endl;
+    cout << "BMI Anda : " << bmi << endl;
+    cout << "Status   : " << statusBMI(bmi) << endl;
+
+    return 0;
+}
